799222451b3991c2fcbd9efe0caf498636c6135c
Renata Maçãs <renata.macas@gmail.com>
2017-09-17 16:05:16 -0300

    ajustes front-end

--- Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/manuygabi/codemenu/MainActivity.java
+++ Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/manuygabi/codemenu/MainActivity.java
@@ -14,19 +14,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      /*  setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       setContentView(R.layout.activity_main);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,6 +38,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);  */
+        return super.onOptionsItemSelected(item);
     }
 }
