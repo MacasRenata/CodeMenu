5c798a2b0c1d5e0b137b317ae8bbc3c89a951a4a
Renata Maçãs <renata.macas@gmail.com>
2017-09-17 17:30:53 -0300

    criação telas

---
+++ Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/manuygabi/codemenu/MenuPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.manuygabi.codemenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.MenuPrincipal);
+
+    }
+
+}
