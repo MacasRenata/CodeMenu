a0fdd8606c4ab814063f4078ba8bc8106e60c424
marciozimmermann <mzsartori@restinga.ifrs.edu.br>
2017-09-27 09:15:44 -0300

    update back-end

---
+++ Desenvolvimento/Back-End/CodeMenuWS/src/java/com/menu/code/webservice/controller/PedidosController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.menu.code.webservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Marcio
+ */
+@RestController
+public class PedidosController {    
+}
