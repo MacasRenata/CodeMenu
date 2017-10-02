19817dfab2ba286c87c507851b4457d0d35386cf
Renata Maçãs <renata.macas@gmail.com>
2017-10-02 12:38:36 -0300

    update webservice

---
+++ Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/macas/codemenu/webservice.java
@@ -0,0 +1,29 @@
+package com.example.macas.codemenu;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+/**
+ * Created by macas on 02/10/17.
+ */
+
+public class webservice {
+
+  /*  public  webservice(){
+
+
+    }
+    public int add (int i, int j) throws IOExeption, XmlPullParserException
+    {
+        SoapObject  soap = new SoapObject (http://calculator.me.org/, “add”);
+        SoapSerializationEnvelope envelope = new  SoapSerializationEnvelope(SoapEnvelope.VER11);
+        HttpTransportSE HttpTrans = new HttpTransportSE(“http://192.xxxxxendereço do webservice bla bla”);
+        HttpTrans.call(“add”, envelope);
+        Object resultado = envelope.getResponse ();
+        return Integer.parseInt(resultado.toString());
+
+    }*/
+
+
+
+
+}
