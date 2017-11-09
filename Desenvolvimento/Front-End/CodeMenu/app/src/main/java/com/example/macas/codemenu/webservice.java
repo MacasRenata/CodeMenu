2d6510f96c0d0f149ffaa899affb5b710b73b517
Renata Maçãs <renata.macas@gmail.com>
2017-11-09 18:49:53 -0200

    teste conexao

--- Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/macas/codemenu/webservice.java
+++ Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/macas/codemenu/webservice.java
@@ -138,55 +138,6 @@ public class webservice {
 
 */
 
-//conexão com mySql NetBeans - ajustar
-
-       /*
-
-    public class WebService {
-
-        private static String readStream(InputStream in){
-            BufferedReader r = new BufferedReader(new InputStreamReader(in));
-            StringBuilder total = new StringBuilder();
-            String line;
-
-            try {
-                while ((line = r.readLine()) != null) {
-                    total.append(line).append('\n');
-                }
-            }catch(Exception ex) {
-                ex.printStackTrace();
-            }
-            return total.toString();
-        }
-
-        private static String request(String stringUrl){
-            URL url = null;
-            HttpURLConnection urlConnection = null;
-            try {
-                url = new URL(stringUrl);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                return readStream(in);
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                urlConnection.disconnect();
-            }
-            return "";
-        }
-
-        public static Localizacao retornarLocalizacaoPorIp(String ip) throws JSONException {
-            String responseBody = request("http://freegeoip.net/json/" + ip);
-            JSONObject obj = new JSONObject(responseBody);
-            String pais = obj.get("country_name").toString();
-            String estado = obj.get("region_name").toString();
-            String cidade = obj.get("city").toString();
-            return new Localizacao(pais, estado, cidade);
-        }
-    }
-*/
 
 
 
