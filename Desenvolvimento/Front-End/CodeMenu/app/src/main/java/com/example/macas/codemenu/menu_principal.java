373d5c0ef07fef3d3c114ae13dd0efcb61cbbe06
Renata Maçãs <renata.macas@gmail.com>
2017-11-13 17:54:16 -0200

    ajuste teste webservice

--- Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/macas/codemenu/menu_principal.java
+++ Desenvolvimento/Front-End/CodeMenu/app/src/main/java/com/example/macas/codemenu/menu_principal.java
@@ -1,7 +1,12 @@
 package com.example.macas.codemenu;
 
-import android.database.Cursor;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.example.macas.codemenu.download_dados;
+import com.example.macas.codemenu.webservice;
+
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
@@ -9,6 +14,8 @@ import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class menu_principal extends AppCompatActivity {
@@ -20,9 +27,12 @@ public class menu_principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal);
 
+        AsyncTask<Void, Void, Void> url = new download_dados().execute();  //download dos dados do webservice
+
         btCardapio = (Button) findViewById(R.id.btCardapio);
 
         btCardapio.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 chamaCardapio();
@@ -34,36 +44,6 @@ public class menu_principal extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-/*
-            public List<cardapio_ref> queryGetAll() {
-                List<cardapio_ref> list = new ArrayList<cardapio_ref>();
-
-                try {
-                    Cursor cursor = this.chamaCardapio (TABLE_NAME,  new String[] {"nomeRef", "imgBD", "valor"},
-                            null, null, null, null, null, null);
-                    int nregistros = cursor.getCount();
-                    if (nregistros!=0) {
-                        cursor.moveToFirst();
-                        do {
-                            Cursor refeicao = new Cursor(cursor.getString(0), cursor.getString(1), cursor.getString(2)) {
-                            };
-                            list.add(refeicao);
-
-                        }while (cursor.moveToNext());
-
-                        if (cursor !=null && ! cursor.isClosed())
-                            cursor.close();
-                        return list;
-                    }else
-                        return null;
-                }
-                catch (Exception err) {
-                    return null;
-                }
-            }
-        });
-*/
-
         });
 
         btPedido = (Button) findViewById(R.id.btPedido);
