f3f54e1e07a7237bf3681b6f2cc3761957412f0f
Renata Maçãs <renata.macas@gmail.com>
2017-12-06 12:23:48 -0200

    ajuste webservice

--- Desenvolvimento/WebApp/CodeMenu/app/src/main/java/com/example/macas/codemenu/cardapioPage.java
+++ Desenvolvimento/WebApp/CodeMenu/app/src/main/java/com/example/macas/codemenu/cardapioPage.java
@@ -1,63 +1,83 @@
 package com.example.macas.codemenu;
 
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ListMenuItemView;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.List;
+import java.util.ListIterator;
+
 
 public class cardapioPage extends AppCompatActivity {
 
-    ListView lv;
-    String id;
-
-    Button btConfirma, btSair;
 
     //talvez fazer metodo da lista aqui
+    private class cardapioPageTask extends AsyncTask<cardapio, Void, Void> {
+
+        @Override
+        public Void doInBackground(cardapio... params) {
+            cardapioDAO dao = new cardapioDAO();
+
+            dao.createNote(params[0]);
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(Void notes) {
+            Intent intent = new Intent();
+
+            intent.putExtra("cardapio", cardapio.class);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        }
+    }
+
+
+    private cardapioDAO cd;
+    private Button getBtConfirma;
+    Button btConfirma, btSair;
+    ListView listaItem;
+
+    //private ContactsContract.CommonDataKinds.Note getOnItemSelectedListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_cardapio);
 
+        this.cd = new cardapioDAO();
+        this.listaItem = (ListView) findViewById(R.id.listaItem);
+      //  this.listaItem.setOnClickListener(listaItem.getAdapter().getItem());
 
-        btConfirma = (Button) findViewById(R.id.btConfirma);
 
-        btConfirma.setOnClickListener(new View.OnClickListener() {
+        this.btConfirma = (Button) findViewById(R.id.btConfirma);
+        this.btConfirma.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                chamaCardapio();
-            }
-
-            void chamaCardapio() {
-                Intent intent = new Intent();
-                intent.setClass(cardapioPage.this, pedidoPage.class);
-                startActivity(intent);
-                finish();
-
+            public void onClick(View view) {
+                pedidoSalvo();
             }
         });
+    }
 
-        btSair = (Button) findViewById(R.id.btSair);
+        private void pedidoSalvo() {
+      //  this.listaItem.setOnClickListener(this.listaItem.getOnItemClickListener(listaItem));
+        //this.note.setText(this.textEdit.getText().toString());
 
-        btSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chamaCardapio();
-            }
-
-            void chamaCardapio() {
-                Intent intent = new Intent();
-                intent.setClass(cardapioPage.this, MainActivityMenu.class);
-                startActivity(intent);
-                finish();
-
-            }
-        });
+     //   new cardapioPageTask().execute(this.listaItem);
 
     }
+
+
 
 }
--- Desenvolvimento/WebApp/CodeMenu/app/src/main/java/com/example/macas/codemenu/pagamentoService.java
+++ Desenvolvimento/WebApp/CodeMenu/app/src/main/java/com/example/macas/codemenu/pagamentoService.java
@@ -1,8 +1,24 @@
 package com.example.macas.codemenu;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 /**
  * Created by macas on 30/11/17.
  */
 
-public class pagamentoService {
+public interface pagamentoService {
+
+    @GET("cardapio")
+    @Headers("x-apikey: a99c54bd6bfa3fb07ef171c1a43f51f29785e")
+    Call<List<cardapio>> listCardapio();
+
+    @POST("cardapio")
+    @Headers("x-apikey: a99c54bd6bfa3fb07ef171c1a43f51f29785e")
+    Call<cardapio> createCardapio(@Body cardapio cardapio);
 }
